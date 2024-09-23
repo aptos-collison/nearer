cf06ff67df234e655dc6c7ab728194811dcdbca6
nwakakukaks <winnernwakaku123@gmail.com>
2024-09-23 14:30:01 +0100

    Add: finished landing page

--- frontend/components/home/features.tsx
+++ frontend/components/home/features.tsx
@@ -25,7 +25,10 @@ export const Features = () => {
                 <img src="/public/icons/euro.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm " />
               </div>
 
-              <p>Effortlessly bridge assets across different networks, simplifying cross-chain transactions.</p>
+              <p>
+                Create bridges to enable assets transfer across different networks, simplifying cross-chain
+                transactions.
+              </p>
             </CardContent>
           </Card>
           <Card className="bg-transparent text-gray-400 border-gray-800 border-2 rounded-none">
@@ -33,8 +36,8 @@ export const Features = () => {
               <CardTitle className="text-white">Token Transfers</CardTitle>
             </CardHeader>
             <CardContent>
-            <img src="/public/icons/token.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3" /> 
-              Easily transfer tokens between wallets, enhancing your trading and investment experience.
+              <img src="/public/icons/token.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3" />
+              Create dapps enabling token transfer between wallets, enhancing your trading and investment experience.
             </CardContent>
           </Card>
           <Card className="bg-transparent text-gray-400 border-gray-800 border-2 rounded-none">
@@ -42,17 +45,17 @@ export const Features = () => {
               <CardTitle className="text-white">Simple Swaps</CardTitle>
             </CardHeader>
             <CardContent>
-            <img src="/public/icons/swap.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3"  />
-              Swap tokens directly on our platform with minimal fees for a convenient trading experience.
+              <img src="/public/icons/swap.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3" />
+              Create dapps to swap tokens on any platform with minimal fees for a convenient trading experience.
             </CardContent>
           </Card>
           <Card className="bg-transparent text-gray-400 border-gray-800 border-2 rounded-none">
             <CardHeader>
-              <CardTitle className="text-white">Faucets for New Users</CardTitle>
+              <CardTitle className="text-white">Faucets for Developers</CardTitle>
             </CardHeader>
             <CardContent>
-            <img src="/public/icons/faucet.svg"className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3" />
-              Start your crypto journey with our faucets, providing free tokens to help you learn the ropes.
+              <img src="/public/icons/faucet.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3" />
+              Build and share faucet dapps to enable developers access your protocol's test tokens across any web platform.
             </CardContent>
           </Card>
           <Card className="bg-transparent text-gray-400 border-gray-800 border-2 rounded-none">
@@ -60,8 +63,8 @@ export const Features = () => {
               <CardTitle className="text-white">Easy Donations</CardTitle>
             </CardHeader>
             <CardContent>
-            <img src="/public/icons/donation.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3" />
-              Support your favorite projects and causes with secure, simple donation options using APT Link.
+              <img src="/public/icons/donation.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3" />
+              Create dapps to support your favorite projects and causes with secure, simple donation options leveraging APT Link.
             </CardContent>
           </Card>
           <Card className="bg-transparent text-gray-400 border-gray-800 border-2 rounded-none">
@@ -69,8 +72,8 @@ export const Features = () => {
               <CardTitle className="text-white">Social Payments</CardTitle>
             </CardHeader>
             <CardContent>
-            <img src="/public/icons/payment.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3" />
-              Send and receive payments effortlessly through social platforms, making transactions feel as easy as
+              <img src="/public/icons/payment.svg" className="h-12 w-12 bg-teal-200 p-2 rounded-sm  ml-1 mb-3" />
+               Create payment dapps to send and receive payments effortlessly through on platforms, making transactions feel as easy as
               chatting.
             </CardContent>
           </Card>
@@ -79,8 +82,8 @@ export const Features = () => {
 
       <Card className="mt-6 py-8 mb-6 bg-gradient-to-b from-teal-100 to-white">
         <CardContent className="flex flex-col gap-3 items-center">
-          <p className="text-4xl font-bold">From start to Dapp within minutes!</p>
-          <p className="font-medium text-gray-600">Explore the endless possibilities with APT-link 🚀</p>
+          <p className="text-5xl font-bold">Create . Deploy . Share</p>
+          <p className="font-medium text-gray-600">Explore the endless possibilities with APT-link 🔗🚀</p>
           <Button onClick={handleGetStarted} size={"lg"} className="rounded-full">
             Get Started Now
           </Button>
